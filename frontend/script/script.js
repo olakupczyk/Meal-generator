8eaa7379ea35abcdfa3f0e4fbf677aee0bff2466
Marek Furik <marekfurik@post.cz>
2022-09-23 12:44:48 +0200

    Small adjusts

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,20 +1,26 @@
-const inputName = document.querySelector('#userName');
-const inputPassword = document.querySelector('#userPassword');
-const btnSumbit = document.querySelector('#btnSubmit');
+const inputName = document.getElementById('#userName');
+const inputPassword = document.getElementById('#userPassword');
+const btnSumbit = document.getElementById('#btnSubmit');
+//const divOutput = document.getElementById ('#btnSubmit');
 
 btnSumbit.addEventListener('click', (e) => {
     
     const payload = {
-        'accountemail': inputName.value,
+        'accountemail': inputName.value,    
         'hashedpassword': inputPassword.value
     }
 
-    const fetchoptions = {
+    const fetchOptions = {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
         },
         body: JSON.stringify(payload)
     }
-fetch('http//127.0.0.1:8738/api/login')
+fetch('http//127.0.0.1:8738/api/login', fetchOptions)
+.then((response) => response.json())
+.then((data) => {
+ //  divOutput
+}) 
+
 })
