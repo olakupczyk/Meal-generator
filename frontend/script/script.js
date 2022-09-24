b9434aaeea2f21f36ea20c96ff60191e6e1d7159
Marek Furik <marekfurik@post.cz>
2022-09-24 12:56:25 +0200

    Script

--- frontend/script/script.js
+++ frontend/script/script.js
@@ -28,13 +28,13 @@
 // const btnSubmit = document.querySelector("#btnBookSubmit");
 // const divOutput = document.querySelector("#divOutput");
 
-const inputEmail = document.querySelector("#userEmail");
-const inputPassword = document.querySelector("#userPassword");
-const btnSubmit = document.querySelector("#btnSubmit");
-const inputAccname = document.querySelector("#accountName");
-const divOutput = document.querySelector("#divOutput");
+//const inputEmail = document.querySelector("#userEmail");
+//const inputPassword = document.querySelector("#userPassword");
+//const btnSubmit = document.querySelector("#btnSubmit");
+//const inputAccname = document.querySelector("#accountName");
+//const divOutput = document.querySelector("#divOutput");
 
-btnSubmit.addEventListener("click", (e) => {
+/* btnSubmit.addEventListener("click", (e) => {
   const payload = {
     "accountemail": inputEmail.value,
 
@@ -68,4 +68,62 @@ function validateForm()
     var pass = document.getElementById('userPassword').value;
     if(pass == `${password}`) //here 1234 is your password
         window.location = 'file:///C:/Jeppe/GitHub/mealGenerator/frontend/register.html';
-}
+} */
+
+const inputEmail = document.querySelector("#userEmail");
+const inputPassword = document.querySelector("#userPassword");
+const btnSubmit = document.querySelector("#btnSubmit");
+
+const btnRegister= document.querySelector('#btnCreateProfile');
+
+const ls = window.localStorage
+
+const port = 8738;
+const baseUrl = `http://127.0.0.1:${port}`;
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  let account;
+  if (ls.getItem('account')) {
+      account = JSON.parse(ls.getItem('account'));
+  }
+  
+  else {
+      btnSubmit.classList.add('hidden');
+      inputEmail.innerHTML = account.loginname;
+
+      if (account.profileid) {
+          btnCreateProfile.classList.add('hidden');
+      } 
+  }
+})
+
+btnSubmit.addEventListener("click", (e) => {
+  const payload = {
+    accountemail: inputEmail.value,
+    password: inputPassword.value
+  }
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  }
+  
+  fetch(`${baseUrl}/api/accounts/login`, fetchOptions)
+  .then(response => {
+      const token = response.headers.get('x-authentication-token');
+      ls.setItem('token', token);
+      console.log(ls.getItem('token'));
+
+      return response.json()
+  })
+  .then(data => {
+      ls.setItem('account', JSON.stringify(data));
+      console.log(ls.getItem('account'));
+
+      window.location.reload();
+  })
+})
+
